faef9de5ba3c875e4319ca9503c89ea6baa1457e
Durran0490 <aleksands_riga@yaho.com>
2019-10-02 19:16:28 +0300

    added method for username input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,8 +13,7 @@ public class Main {
 
         int myNum = rand.nextInt(100) + 1;
         boolean answer;
-
-        String PlayerName = userName("What is your name?");
+        String PlayerName = userName("What is your name?\n(please input at least three characters)", 3, 10);
 
         do {
             boolean userWon = false;
@@ -24,24 +23,22 @@ public class Main {
 
                 if (userNum == myNum) {
                     userWon = true;
-                    System.out.println("Requested number: " + myNum);
-                    System.out.println("Congratulations, you are right");
+                    System.out.println("Number is: " + myNum + " !");
+                    System.out.println("Congratulations, you are right!");
                     break;
                 } else if (myNum > userNum) {
-                    System.out.println("Requested number is bigger ");
-                    System.out.println("Guess again");
+                    System.out.println("Number is greater");
                 } else if (myNum < userNum) {
-                    System.out.println("Requested number is lesser ");
-                    System.out.println("Guess again");
+                    System.out.println("Number is smaller");
                 }
             }
+
             if (!userWon) {
-                System.out.println("Requested number: " + myNum);
+                System.out.println("Number is: " + myNum + "!");
                 System.out.println("Sorry, you're out of luck");
             }
         } while (choseOption("Do you want to play more? (Y/N)"));
-
-        System.out.println("Goodbye" + PlayerName);
+        System.out.println("Goodbye " + '"' + PlayerName + '"' + "!");
     }
 
     static int askInt(String msg, int min, int max) {
@@ -49,11 +46,12 @@ public class Main {
             try {
                 System.out.println(msg);
                 int answer = scn.nextInt();
+
                 if (answer >= min && answer <= max) {
                     return answer;
                 }
             } catch (InputMismatchException ex) {
-                System.out.println("It isn't a number");
+                System.out.println("It isn't a number!");
                 scn.next();
             }
             System.out.printf("Please enter number from %d to %d\n", min, max);
@@ -66,6 +64,7 @@ public class Main {
             String option = scn.next();
             boolean isY = option.equalsIgnoreCase("y");
             boolean isN = option.equalsIgnoreCase("n");
+
             if (isY || isN) {
                 return isY;
             }
@@ -73,15 +72,24 @@ public class Main {
         }
     }
 
-    static String userName(String msg) {
+    static String userName(String msg, int min, int max) {
         System.out.println(msg);
         while (true) {
-                String name = scn.nextLine();
-                System.out.println("Welcome!");
+            String name = scn.nextLine();
+            int nameLength = name.length();
+
+            if (nameLength > max) {
+                System.out.printf("Sorry, but your name is to long!\nPlease input less than a %d characters\n", max);
+            } else if (nameLength < min) {
+                System.out.printf("Sorry, but your name is to short!\nPlease input at least %d characters\n", min);
+            } else {
+                System.out.println("Welcome " + '"' + name + '"' + "!");
                 return name;
             }
         }
     }
+}
+
 
 
 
