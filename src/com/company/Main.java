27cb7c1f60bd540a0708f61943ffd1cd385c6271
Durran0490 <aleksands_riga@yahoo.com>
2019-10-09 21:58:43 +0300

    minor changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,42 +9,48 @@ public class Main {
     static Random rand = new Random();
     static Scanner scn = new Scanner(System.in);
 
-    //--Main body
 
     public static void main(String[] args) {
+        System.out.println("Hello and Welcome to the " + '"' + "GUESS THE NUMBER" + '"');
 
-        String PlayerName = userName("Hello and Welcome!\nWhat's your name?\n" +
-                "(please input from 3 to 10 characters without spaces)", 3, 10);
+        String playerName = askPlayerName("What's your name?\n" +
+                "(min 3 characters without spaces)", 3, 15);
 
         do {
-            int myNum = rand.nextInt(100) + 1;
-            boolean userWon = false;
-            int NumOfAttempts = 9;
-
-            for (int i = 0; i <= NumOfAttempts; i++) {
-                int userNum = askInt("Enter your guess: ", 1, 100);
-
-                if (userNum == myNum) {
-                    userWon = true;
-                    System.out.println("Number is " + myNum + " !");
-                    System.out.println("Congratulations, you are right!");
-                    break;
-                } else if (myNum > userNum) {
-                    System.out.println("Number is greater\n Attempts left: " + (NumOfAttempts - i));
-                } else if (myNum < userNum) {
-                    System.out.println("Number is smaller\n Attempts left: " + (NumOfAttempts - i));
-                }
-            }
-
-            if (!userWon) {
-                System.out.println("Number is " + myNum + "!");
-                System.out.println("Sorry, you're out of luck");
-            }
+            playGame(10);
         } while (choseOption("Do you want to play again? (Y/N)"));
-        System.out.println("Goodbye " + '"' + PlayerName + '"' + "!");
+        System.out.println("Goodbye " + '"' + playerName + '"' + " !");
     }
 
-    //--Asking and checking if it's a number from a player
+    //-- Game itself
+    static int playGame(int numOfAttempts) {
+        int myNum = rand.nextInt(100) + 1;
+        boolean userWon = false;
+
+        for (int i = 0; i < numOfAttempts; i++) {
+            System.out.print("Try " + (i + 1) + ": ");
+            int userNum = askInt("Enter your guess: ", 1, 100);
+
+            if (userNum == myNum) {
+                userWon = true;
+                System.out.println("Number is " + myNum + " !");
+                System.out.println("Congratulations, you are right!");
+                break;
+            } else if (myNum > userNum) {
+                System.out.println("Number is greater");
+            } else if (myNum < userNum) {
+                System.out.println("Number is smaller");
+            }
+        }
+
+        if (!userWon) {
+            System.out.println("Number is " + myNum + "!");
+            System.out.println("Sorry, you're out of luck");
+        }
+        return 0;
+    }
+
+    //--Asking and checking if it's a number, from a player
 
     static int askInt(String msg, int min, int max) {
         while (true) {
@@ -63,7 +69,7 @@ public class Main {
         }
     }
 
-    //--Checking for specific strings if player wants to continue
+    //--Checking if player wants to continue
 
     static boolean choseOption(String msg) {
         while (true) {
@@ -81,7 +87,7 @@ public class Main {
 
     //--Asking player's name
 
-    static String userName(String msg, int min, int max) {
+    static String askPlayerName(String msg, int min, int max) {
         System.out.println(msg);
         while (true) {
             String name = scn.nextLine();
@@ -93,11 +99,12 @@ public class Main {
             } else if (nameLength < min) {
                 System.out.printf("Sorry, but your name is to short!\nPlease input at least %d characters\n", min);
             } else {
-                System.out.println("Greetings " + '"' + name + '"' + "!");
+                System.out.println("Greetings " + '"' + name + '"' + "!\n");
                 return name;
             }
         }
     }
+
 }
 
 
